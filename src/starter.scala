d6403693eec4f60d3450a29ed83465f012c523a4
Leszek Wlodarski <leszek.wlodarski@mbank.pl>
2014-06-08 17:59:49 +0200

    problem11 solved

--- src/starter.scala
+++ src/starter.scala
@@ -17,11 +17,50 @@ object starter extends App {
 	    sumAll( l, 0 )
 	  }
 	  
-	  val max = 2000000
-	  val startTime = System.currentTimeMillis()
-	  println( "Sum of all primes less then " + max + " equals " + problem10( max) )
-	  val endTime = System.currentTimeMillis()
-      val dur = endTime - startTime
-      println(dur + " msecs")
+	  def main_problem10 = {
+		  val max = 2000000
+		  val startTime = System.currentTimeMillis()
+		  println( "Sum of all primes less then " + max + " equals " + problem10( max) )
+		  val endTime = System.currentTimeMillis()
+	      val dur = endTime - startTime
+	      println(dur + " msecs")
+	  }
+	  
+	  def problem11(data: Array[ Array[Int]], width:Int, height:Int, length:Int) : Long = {
+	    def max(a: Long, b: Long) = if (a>b) a else b
+	    
+	    def vertical(x: Int, y:Int, l: Int): Long = if (l==0) 1 else if (x>=width || y>=height) 0 else data(x)(y) * vertical(x, y+1, l-1) 
+	    
+	    def horizontal(x: Int, y:Int, l: Int): Long = if (l==0) 1 else if (x>=width || y>=height) 0 else data(x)(y) * horizontal(x+1, y, l-1)
+	    
+	    def diagonal1(x: Int, y:Int, l: Int): Long = if (l==0) 1 else if (x>=width|| y>=height) 0 else data(x)(y) * diagonal1(x+1, y+1, l-1)
+	    def diagonal2(x: Int, y:Int, l: Int): Long = if (l==0) 1 else if (x<0 || x>=width || y>=height) 0 else data(x)(y) * diagonal2(x-1, y+1, l-1)
+	    
+		def problem11Impl(x: Int, y: Int, length: Int): Long = 
+		{	
+	     val cur =  max( max( vertical(x, y, length), horizontal(x,y, length)), max(diagonal1(x, y, length), diagonal2(x,y, length)) )
+	     if (x<width)
+	       max(cur, problem11Impl(x+1, y, length))
+	     else
+	     if (y<height)
+	       max(cur, problem11Impl(0, y+1, length))
+	     else
+	       0
+		}
+		
+		problem11Impl(0, 0, 4)
+	  }
+	  
+	  def main_problem11 = {
+		  val max = 2000000
+		  val startTime = System.currentTimeMillis()
+		  println( "Largest product equals " + problem11(Problem11.Data, 20, 20, 4) )
+		  val endTime = System.currentTimeMillis()
+	      val dur = endTime - startTime
+	      println(dur + " msecs")
+	  }
+	  
+	  main_problem11
+	  
 	  
 }
