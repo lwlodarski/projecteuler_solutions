332b4a8113c4c9e88d737af7461d5021dc993059
Leszek Wlodarski <leszek.wlodarski@mbank.pl>
2014-06-08 09:39:34 +0200

    Problem with wrong result type solved, still it is slow

--- src/starter.scala
+++ src/starter.scala
@@ -1,20 +1,20 @@
 object starter extends App {
 
 	  //sum of all prime number less than max
-	  def problem10(max: Int) : Int = {
-	    def isPrime(no: Int, primes: List[Int]) : Boolean = if (primes.isEmpty) true else if (no % primes.head==0) false else isPrime(no, primes.tail)
+	  def problem10(max: Long) : Long = {
+	    def isPrime(no: Long, primes: List[Long], sqr: Long) : Boolean = if (primes.isEmpty || primes.head>sqr) true else if (no % primes.head==0) false else isPrime(no, primes.tail, sqr)
 	    
-	    def problemImpl(cur: Int, primes: List[Int]) : List[Int] = 
+	    def problemImpl(cur: Long, primes: List[Long]) : List[Long] = 
 	      if (cur>=max) primes else
-	        if (isPrime(cur, primes)) {
-	          problemImpl(cur+1, primes :+ cur)
+	        if (isPrime(cur, primes, Math.sqrt(cur).toLong)) {
+	          problemImpl(cur+1, cur :: primes)
 	        } else problemImpl(cur+1, primes)
 	        
-	    def sumAll(list: List[Int]) : Int = if (list.isEmpty) 0 else list.head + sumAll(list.tail)
+	    def sumAll(list: List[Long], result:Long) : Long = if (list.isEmpty) result else sumAll(list.tail, result + list.head)
 	        
 	    val l = problemImpl(3, 2 :: Nil)
-	    println(l)
-	    sumAll( l )
+	    println("All elements count = " + l.length)
+	    sumAll( l, 0 )
 	  }
 	  
 	  val max = 2000000
